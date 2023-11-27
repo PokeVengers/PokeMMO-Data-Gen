6a976d00085da50805edee6c972a5f24fd9ac387
Gamer2020 <lostatlife2020@hotmail.com>
2023-11-27 11:34:04 -0500

    formatting fix.

--- download_PokeAPI_egg-group.py
+++ download_PokeAPI_egg-group.py
@@ -27,26 +27,30 @@ EGG_GROUP_NAME_LOOKUP = {
     # Add any other egg groups that PokéMMO uses which aren't listed in the PokeAPI
 }
 
+
 def save_data(data, file_name):
-    with open(DATA_SAVE_PATH + file_name, 'w', encoding='utf-8') as file:
+    with open(DATA_SAVE_PATH + file_name, "w", encoding="utf-8") as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
+
 
 def is_in_first_five_generations(species_id):
     response = requests.get(POKEMON_SPECIES_BASE_URL + str(species_id))
     if response.status_code == 200:
         species_data = response.json()
-        generation_number = int(species_data['generation']['url'].split('/')[-2])
+        generation_number = int(species_data["generation"]["url"].split("/")[-2])
         return generation_number <= 5
     return False
+
 
 def process_pokemon_species(pokemon_species_list):
     filtered_species = []
     for species in pokemon_species_list:
-        species_id = species['url'].split('/')[-2]
+        species_id = species["url"].split("/")[-2]
         if is_in_first_five_generations(species_id):
-            species.pop('url', None)
+            species.pop("url", None)
             filtered_species.append(species)
     return filtered_species
+
 
 def get_egg_group_data():
     all_egg_groups = {}
@@ -57,7 +61,7 @@ def get_egg_group_data():
         print("Failed to retrieve data")
         return
 
-    total_egg_groups = response.json()['count']
+    total_egg_groups = response.json()["count"]
 
     # Loop through all egg groups
     for i in range(1, total_egg_groups + 1):
@@ -66,28 +70,34 @@ def get_egg_group_data():
             egg_group_data = egg_group_response.json()
 
             # Replace the egg group name using the lookup table
-            egg_group_name = egg_group_data['name']
+            egg_group_name = egg_group_data["name"]
             if egg_group_name in EGG_GROUP_NAME_LOOKUP:
                 egg_group_name = EGG_GROUP_NAME_LOOKUP[egg_group_name]
-                egg_group_data['name'] = egg_group_name
+                egg_group_data["name"] = egg_group_name
             else:
-                print(f"Warning: Egg group name '{egg_group_name}' not found in lookup table")
+                print(
+                    f"Warning: Egg group name '{egg_group_name}' not found in lookup table"
+                )
 
             # Remove the 'names' field
-            egg_group_data.pop('names', None)
+            egg_group_data.pop("names", None)
 
             # Process and filter pokemon_species for generations 1-5
-            if 'pokemon_species' in egg_group_data:
-                egg_group_data['pokemon_species'] = process_pokemon_species(egg_group_data['pokemon_species'])
+            if "pokemon_species" in egg_group_data:
+                egg_group_data["pokemon_species"] = process_pokemon_species(
+                    egg_group_data["pokemon_species"]
+                )
 
             # Add modified egg group data to all egg groups
             all_egg_groups[f"egg_group_{i}"] = egg_group_data
 
     return all_egg_groups
 
+
 def main():
     all_egg_groups_data = get_egg_group_data()
     save_data(all_egg_groups_data, ALL_EGG_GROUPS_FILE)
+
 
 if __name__ == "__main__":
     main()
