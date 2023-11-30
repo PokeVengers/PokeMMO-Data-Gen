35e908d633186d85ac6fd170af447099599d5686
Gamer2020 <lostatlife2020@hotmail.com>
2023-11-29 22:58:43 -0500

    Script that can run all files at once.

---
+++ generate_all_files.py
@@ -0,0 +1,19 @@
+import subprocess
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+def run_script(script_path):
+    """Runs a Python script at the given path."""
+    try:
+        result = subprocess.run(['python', script_path], check=True, text=True, capture_output=True)
+        print(f"Script {script_path} executed successfully.")
+        print("Output:\n", result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error in script {script_path}: {e}")
+
+# List of scripts to run
+scripts_to_run = [os.path.join(current_dir, 'download_PokeAPI_pokemon.py'),os.path.join(current_dir, 'download_PokeAPI_egg-group.py')]
+
+for script in scripts_to_run:
+    run_script(script)
