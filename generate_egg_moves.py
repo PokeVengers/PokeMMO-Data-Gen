4da260044d01addfe716beb32f1dfd42a84bc733
Gamer2020 <lostatlife2020@hotmail.com>
2023-12-04 12:41:58 -0500

    Attempt to add optimizations.

--- generate_egg_moves.py
+++ generate_egg_moves.py
@@ -19,49 +19,43 @@ def extract_egg_moves(pokemon_data):
             egg_moves[pokemon] = []
     return egg_moves
 
-def can_breed(pokemon1, pokemon2, all_pokemon):
+def can_breed(pokemon1, pokemon2, egg_groups, gender_rates):
     """ Check if two Pokémon can breed based on their egg groups and gender rates. """
-    if all_pokemon[pokemon1]['gender_rate'] == -1 and pokemon2 not in [pokemon1, 'ditto']:
+    if gender_rates[pokemon1] == -1 and pokemon2 not in [pokemon1, 'ditto']:
         return False
-    if all_pokemon[pokemon2]['gender_rate'] == -1 and pokemon1 not in [pokemon2, 'ditto']:
+    if gender_rates[pokemon2] == -1 and pokemon1 not in [pokemon2, 'ditto']:
         return False
-    egg_groups1 = set(all_pokemon[pokemon1]['egg_groups'])
-    egg_groups2 = set(all_pokemon[pokemon2]['egg_groups'])
-    return egg_groups1.intersection(egg_groups2) and "cannot-breed" not in egg_groups1
+    return egg_groups[pokemon1].intersection(egg_groups[pokemon2]) and "cannot-breed" not in egg_groups[pokemon1]
 
-def find_breeding_partners(pokemon, all_pokemon):
+def find_breeding_partners(pokemon, all_pokemon, egg_groups, gender_rates):
     """ Find all possible breeding partners for a given Pokémon. """
-    return [p for p in all_pokemon if can_breed(pokemon, p, all_pokemon)]
+    return [p for p in all_pokemon if can_breed(pokemon, p, egg_groups, gender_rates)]
 
 # Memoization cache for breeding chains
 breeding_chain_cache = {}
 
-def breeding_chain(pokemon, move, all_pokemon, current_chain, visited=set(), depth=0, max_depth=3):
+def breeding_chain(pokemon, move, all_pokemon, current_chain, visited, depth=0, max_depth=10):
     """ Recursively find breeding chains for a specific move. """
-    # Avoid infinite recursion by checking if we have already visited this pokemon
-    if pokemon in visited:
+    if pokemon in visited or depth > max_depth:
         return []
     visited.add(pokemon)
 
-    # Check if 'moves' key exists
-    if 'moves' not in all_pokemon[pokemon]:
-        return []
-
     cache_key = (pokemon, move, tuple(current_chain))
     if cache_key in breeding_chain_cache:
+        visited.remove(pokemon)  # Remove pokemon from visited before returning
         return breeding_chain_cache[cache_key]
 
-    if move in [m['name'] for m in all_pokemon[pokemon]['moves'] if m['type'] != 'egg_moves']:
-        return [current_chain]
+    if 'moves' not in all_pokemon[pokemon] or not any(m['name'] == move and m['type'] != 'egg_moves' for m in all_pokemon[pokemon]['moves']):
+        visited.remove(pokemon)
+        return []
 
-    potential_breeders = find_breeding_partners(pokemon, all_pokemon)
     chains = []
-    for breeder in potential_breeders:
+    for breeder in breeding_partners[pokemon]:
         if breeder not in visited:
-            new_chain = current_chain + [breeder]
-            chains.extend(breeding_chain(breeder, move, all_pokemon, new_chain, visited.copy(), depth + 1, max_depth))
+            chains.extend(breeding_chain(breeder, move, all_pokemon, current_chain + [breeder], visited, depth + 1, max_depth))
 
     breeding_chain_cache[cache_key] = chains
+    visited.remove(pokemon)
     return chains
 
 def determine_breeding_chains(all_pokemon, egg_moves):
@@ -70,7 +64,7 @@ def determine_breeding_chains(all_pokemon, egg_moves):
     for pokemon, moves in egg_moves.items():
         breeding_chains[pokemon] = {}
         for move in moves:
-            breeding_chains[pokemon][move] = breeding_chain(pokemon, move, all_pokemon, [pokemon], set(), 0, max_depth=3)
+            breeding_chains[pokemon][move] = breeding_chain(pokemon, move, all_pokemon, [pokemon], set(), 0, max_depth=10)
     return breeding_chains
 
 def write_json(data, filename):
@@ -79,16 +73,16 @@ def write_json(data, filename):
         json.dump(data, file, ensure_ascii=False, indent=4)
 
 def main():
-    # Load the original Pokémon data
     pokemon_data = load_json(INPUT_FILE)
 
-    # Extract egg moves for each Pokémon
+    egg_groups = {pokemon: set(data['egg_groups']) for pokemon, data in pokemon_data.items()}
+    gender_rates = {pokemon: data['gender_rate'] for pokemon, data in pokemon_data.items()}
+    global breeding_partners
+    breeding_partners = {pokemon: find_breeding_partners(pokemon, pokemon_data, egg_groups, gender_rates) for pokemon in pokemon_data}
+
     egg_moves = extract_egg_moves(pokemon_data)
+    breeding_chains = {pokemon: {move: breeding_chain(pokemon, move, pokemon_data, [pokemon], set()) for move in moves} for pokemon, moves in egg_moves.items()}
 
-    # Determine breeding chains for each egg move
-    breeding_chains = determine_breeding_chains(pokemon_data, egg_moves)
-
-    # Output the data to a new JSON file
     write_json(breeding_chains, OUTPUT_FILE)
 
 if __name__ == "__main__":
