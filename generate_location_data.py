940a2cb10179dc943312cfca79dcd6bf8395d5d4
Gamer2020 <lostatlife2020@hotmail.com>
2024-01-14 15:42:32 -0500

    Location data rework.

--- generate_location_data.py
+++ generate_location_data.py
@@ -1,20 +1,27 @@
 import json
 import os
 
+
 def read_pokemon_data(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         return json.load(file)
+
 
 def generate_location_data(pokemon_data):
     location_data = {}
     for pokemon, data in pokemon_data.items():
-        pokemon_id = data['id']  # Extract the Pokémon ID
-        for encounter in data.get('location_area_encounters', []):
-            replacements = {' ': '_', '/': '_', '(': '_', ')': '_'}  # Add more replacements as needed
-            location = ''.join(replacements.get(char, char) for char in encounter['location'])
-            display_name = encounter['location']  # Store the original location name
-            if location not in location_data:
-                location_data[location] = {"name": display_name, "encounters": []}
+        pokemon_id = data["id"]  # Extract the Pokémon ID
+        for encounter in data.get("location_area_encounters", []):
+            region = encounter.get("region_name", "").upper()
+            location_name = encounter["location"].replace(" ", "_").upper()
+            location_key = f"{region}_{location_name}"  # Combine region and location
+
+            if location_key not in location_data:
+                location_data[location_key] = {
+                    "name": encounter["location"],
+                    "region": region,  # Add region field
+                    "encounters": [],
+                }
             encounter_info = {
                 "pokemon": pokemon,
                 "pokemon_id": pokemon_id,  # Include the Pokémon ID
@@ -23,14 +30,16 @@ def generate_location_data(pokemon_data):
                 "region_name": encounter.get("region_name"),
                 "min_level": encounter.get("min_level"),
                 "max_level": encounter.get("max_level"),
-                "rarity": encounter.get("rarity")
+                "rarity": encounter.get("rarity"),
             }
-            location_data[location]["encounters"].append(encounter_info)
+            location_data[location_key]["encounters"].append(encounter_info)
     return location_data
 
+
 def save_location_data(location_data, file_path):
-    with open(file_path, 'w', encoding='utf-8') as file:
+    with open(file_path, "w", encoding="utf-8") as file:
         json.dump(location_data, file, ensure_ascii=False, indent=4)
+
 
 def main():
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -50,6 +59,7 @@ def main():
     save_location_data(location_data, location_data_path)
 
     print(f"Location data saved to {location_data_path}")
+
 
 if __name__ == "__main__":
     main()
