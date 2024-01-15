e4d3619a9901fd771d8dda72abf53849f6e2fc51
Gamer2020 <lostatlife2020@hotmail.com>
2024-01-14 19:37:45 -0500

    Now properly renames keys

--- patch_data_files.py
+++ patch_data_files.py
@@ -3,46 +3,65 @@ import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 DATA_SAVE_PATH = "./data/"
-POKEMON_DATA_FILE = 'pokemon-data.json'
-POKEMON_PATCH_FILE = 'patch_pokemon-data.json'
-ABILITIES_DATA_FILE = 'abilities-data.json'
-ITEM_DATA_FILE = 'item-data.json'
-MOVES_DATA_FILE = 'moves-data.json'
-MOVES_PATCH_FILE = 'patch_move-data.json'
+POKEMON_DATA_FILE = "pokemon-data.json"
+POKEMON_PATCH_FILE = "patch_pokemon-data.json"
+ABILITIES_DATA_FILE = "abilities-data.json"
+ITEM_DATA_FILE = "item-data.json"
+MOVES_DATA_FILE = "moves-data.json"
+MOVES_PATCH_FILE = "patch_move-data.json"
+
 
 def load_json(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, "r", encoding="utf-8") as file:
         return json.load(file)
 
+
 def update_nested_key(data, key_path, value):
-    keys = key_path.split('.')
+    keys = key_path.split(".")
     for key in keys[:-1]:
         data = data.setdefault(key, {})
     data[keys[-1]] = value
 
+
 def apply_patch(original_data, patch_data):
     for key, changes in patch_data.items():
         for change_key, value in changes.items():
-            if '_add' in change_key:
-                list_key = change_key.replace('_add', '')
+            if "_add" in change_key:
+                list_key = change_key.replace("_add", "")
                 if list_key in original_data[key]:
                     original_data[key][list_key].extend(value)
-            elif '_remove' in change_key:
-                list_key = change_key.replace('_remove', '')
+            elif "_remove" in change_key:
+                list_key = change_key.replace("_remove", "")
                 if list_key in original_data[key]:
-                    original_data[key][list_key] = [item for item in original_data[key][list_key] if item not in value]
-            elif '.' in change_key:
+                    original_data[key][list_key] = [
+                        item
+                        for item in original_data[key][list_key]
+                        if item not in value
+                    ]
+            elif "." in change_key:
                 update_nested_key(original_data[key], change_key, value)
             else:
                 original_data[key][change_key] = value
 
+
 def replace_string_in_data(data, old_string, new_string):
     if isinstance(data, dict):
-        for key, value in data.items():
+        for key in list(
+            data.keys()
+        ):  # Use list to avoid RuntimeError due to change in dictionary size during iteration
+            new_key = key.replace(old_string, new_string) if old_string in key else key
+            value = data[key]
             if isinstance(value, (dict, list)):
                 replace_string_in_data(value, old_string, new_string)
-            elif isinstance(value, str) and old_string in value:
-                data[key] = value.replace(old_string, new_string)
+            elif isinstance(value, str):
+                value = value.replace(old_string, new_string)
+
+            if new_key != key:
+                data[new_key] = value
+                del data[key]
+            else:
+                data[key] = value
+
     elif isinstance(data, list):
         for i, item in enumerate(data):
             if isinstance(item, (dict, list)):
@@ -50,9 +69,11 @@ def replace_string_in_data(data, old_string, new_string):
             elif isinstance(item, str) and old_string in item:
                 data[i] = item.replace(old_string, new_string)
 
+
 def save_json(data, filename):
-    with open(filename, 'w', encoding='utf-8') as file:
+    with open(filename, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
+
 
 def main():
     # Process Pokemon Data
@@ -80,6 +101,7 @@ def main():
     moves_patch_data = load_json(os.path.join(current_dir, MOVES_PATCH_FILE))
     apply_patch(moves_original_data, moves_patch_data)
     save_json(moves_original_data, DATA_SAVE_PATH + MOVES_DATA_FILE)
+
 
 if __name__ == "__main__":
     main()
